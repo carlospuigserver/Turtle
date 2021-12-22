c009cb7804dbd30d085a1e7e7176891deb19fd5e
carlospuigserver <cpuiggut@myuax.com>
2021-12-22 17:10:24 +0100

    decagono

---
+++ Decágono.py
@@ -0,0 +1,12 @@
+import turtle
+turtle.Turtle()
+l=100
+a=36
+turtle.speed(0)
+turtle.bgcolor("green")
+turtle.color("orange")
+turtle.width(6)
+for _ in range(10):
+    turtle.forward(l)
+    turtle.left(a)
+turtle.done()
