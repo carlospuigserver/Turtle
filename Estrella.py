56f2166c2709fe016e052e44083dae1fefa97d6c
carlospuigserver <cpuiggut@myuax.com>
2021-12-22 20:16:04 +0100

    Estrella 9 puntas

---
+++ Estrella.py
@@ -0,0 +1,12 @@
+import turtle
+turtle.Turtle
+l=200
+a=160
+turtle.speed(0)
+turtle.bgcolor("blue")
+turtle.color("red")
+turtle.width(35)
+for _ in range(9):
+    turtle.forward(l)
+    turtle.left(a)
+turtle.done()
