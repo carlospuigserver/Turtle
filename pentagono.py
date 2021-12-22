b27e3e50b32ca73d6c4f9bcb10bd4942de4da843
carlospuigserver <cpuiggut@myuax.com>
2021-12-22 16:46:31 +0100

    pentagono

---
+++ pentagono.py
@@ -0,0 +1,10 @@
+# Angulo pentagono= 360/5
+import turtle
+turtle.Turtle()
+longitud=100
+angulo=72
+turtle.speed(0)
+for _ in range(5):
+    turtle.forward(longitud)
+    turtle.left(angulo)
+turtle.done()    
